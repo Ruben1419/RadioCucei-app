bb5e856c6e7f3e639f270ee706f5f7396d2435fb
Curuval1 <127057925+Curuval1@users.noreply.github.com>
2025-04-08 16:23:37 -0600

    Navegation

--- components/ProgramaCard.js
+++ components/ProgramaCard.js
@@ -1,64 +1,64 @@
 import React from 'react';
 import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
 
-const ProgramaCard = ({ image, title, description, locutores }) => {
-  return (
-    <View style={styles.card}>
-      <Image source={image} style={styles.image} />
-      <Text style={styles.title}>{title}</Text>
-      <Text style={styles.description}>{description}</Text>
-      <Text style={styles.locutores}>{locutores}</Text>
-      <TouchableOpacity style={styles.button}>
-        <Text style={styles.buttonText}>Ver más</Text>
-      </TouchableOpacity>
-    </View>
-  );
+const ProgramaCard = ({ image, title, description, locutores, onbuttonPress }) => {
+    return (
+        <View style={styles.card}>
+            <Image source={image} style={styles.image} />
+            <Text style={styles.title}>{title}</Text>
+            <Text style={styles.description}>{description}</Text>
+            <Text style={styles.locutores}>{locutores}</Text>
+            <TouchableOpacity style={styles.button} onPress={onbuttonPress}>
+                <Text style={styles.buttonText}>Ver más</Text>
+            </TouchableOpacity>
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
-  card: {
-    backgroundColor: '#fff',
-    borderRadius: 20,
-    padding: 10,
-    marginBottom: 10,
-    marginHorizontal: 10,
-    alignItems: 'center',
-    width: 150, // Ajusta el ancho de la tarjeta
-    justifyContent: 'space-between',
-  },
-  image: {
-    width: 60, // Ajusta el ancho de la imagen
-    height: 60, // Ajusta la altura de la imagen
-    resizeMode: 'contain',
-  },
-  title: {
-    fontSize: 14, // Ajusta el tamaño de la fuente del título
-    fontWeight: 'bold',
-    marginTop: 4,
-  },
-  description: {
-    fontSize: 10, // Ajusta el tamaño de la fuente de la descripción
-    textAlign: 'center',
-    marginTop: 4,
-  },
-  locutores: {
-    fontSize: 10, // Ajusta el tamaño de la fuente de los locutores
-    color: '#888',
-    marginTop: 4,
-  },
-  button: {
-    backgroundColor: '#007BFF',
-    paddingVertical: 6,
-    paddingHorizontal: 12,
-    borderRadius: 4,
-    marginTop: 8,
-    alignContent: 'bottom',
-    
-  },
-  buttonText: {
-    color: '#fff',
-    fontSize: 12,
-  },
+    card: {
+        width: '48%',
+        backgroundColor: '#fff',
+        borderRadius: 10,
+        padding: 10,
+        marginBottom: 20,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2,
+        shadowRadius: 4,
+        elevation: 5,
+    },
+    image: {
+        width: '100%',
+        height: 100,
+        borderRadius: 10,
+        marginBottom: 10,
+    },
+    title: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        marginBottom: 5,
+    },
+    description: {
+        fontSize: 14,
+        color: '#555',
+        marginBottom: 5,
+    },
+    locutores: {
+        fontSize: 12,
+        color: '#777',
+        marginBottom: 10,
+    },
+    button: {
+        backgroundColor: '#FFD700',
+        paddingVertical: 8,
+        borderRadius: 5,
+        alignItems: 'center',
+    },
+    buttonText: {
+        color: '#000',
+        fontWeight: 'bold',
+    },
 });
 
 export default ProgramaCard;
